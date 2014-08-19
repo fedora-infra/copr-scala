280ef8f14e66356a8b89effb999326b95ed450de
Ricky Elrod <ricky@elrod.me>
2014-08-19 02:45:56 -0400

    Add an example using lenses
    Signed-off-by: Ricky Elrod <ricky@elrod.me>

--- src/test/scala/CoprTests.scala
+++ src/test/scala/CoprTests.scala
@@ -4,8 +4,12 @@ import org.scalatest.FunSuite
 
 import argonaut._, Argonaut._
 import me.elrod.copr._, Copr._
+import monocle.function.At._
+import monocle.std._
+import monocle.syntax._
 import io.Source
-import scalaz._, Scalaz._
+import scala.language.postfixOps
+import scalaz.{ Lens => _, _ }
 import scalaz.concurrent.Task
 import scalaz.concurrent.Task._
 
@@ -72,6 +76,25 @@ class CoprTests extends FunSuite {
           assertResult("ok", ".output")(build.output)
           assertResult(Some("succeeded"), "epel-7-x86_64 chroot")(build.chroots.get("epel-7-x86_64"))
           assertResult("codeblock", ".submittedBy")(build.submittedBy)
+        }
+        case -\/(err) => println(err)
+      }
+    }.run
+  }
+
+  test("Can get detail about one of my builds using lenses") {
+    import BuildDetail._
+    val r: Task[String \/ BuildDetail] = for {
+      c <- config
+      resp <- buildDetail(c)(27256)
+    } yield resp
+
+    r.map { rr =>
+      rr match {
+        case \/-(build) => {
+          assertResult("ok", ".output")(build |-> output get)
+          assertResult(Some("succeeded"), "epel-7-x86_64 chroot")(build |-> chroots |-> at("epel-7-x86_64") get)
+          assertResult("codeblock", ".submittedBy")(build |-> submittedBy get)
         }
         case -\/(err) => println(err)
       }
